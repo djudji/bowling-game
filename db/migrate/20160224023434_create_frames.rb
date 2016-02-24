15a18517fc1eb71ff612a2330e4e423ae0798564
Zlatko Alomerovic <zlatko.alomerovic@gmail.com>
2016-02-24 20:01:11 +0000

    Created initial models

--- db/migrate/20160224023434_create_frames.rb
+++ db/migrate/20160224023434_create_frames.rb
@@ -2,10 +2,10 @@ class CreateFrames < ActiveRecord::Migration
   def change
     create_table :frames do |t|
       t.references :game, index: true, foreign_key: true
-      t.int :ball_one
-      t.int :ball_two
-      t.int :ball_three
-      t.int :frame_score
+      t.integer :ball_one
+      t.integer :ball_two
+      t.integer :ball_three
+      t.integer :frame_score
 
       t.timestamps null: false
     end
