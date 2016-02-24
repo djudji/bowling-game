0bf7aac74a675044d94d7386b78ae2fc3e8b9b6b
Zlatko Alomerovic <zlatko.alomerovic@gmail.com>
2016-02-24 15:46:48 +0000

    Added basic description to models

--- app/models/frame.rb
+++ app/models/frame.rb
@@ -1,3 +1,6 @@
 class Frame < ActiveRecord::Base
   belongs_to :game
+  validates :ball_one, :ball_two, :ball_three, 
+            numericality: { only_integer: true, less_than_or_equal_to: 10}
+  
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,6 @@
 class Game < ActiveRecord::Base
   has_many :frames
+  validates_associated :frames
+  validates :current_frame, numericality: { only_integer: true, less_than_or_equal_to: 10}
+  
 end
